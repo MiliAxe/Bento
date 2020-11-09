d2486117b396dd4f5bf764d1bbb73cb3e03a1814
MiliAxe <55515322+MiliAxe@users.noreply.github.com>
2020-11-09 17:18:07 +0330

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -6,12 +6,12 @@ var hour = today.getHours();
 var name = 'John Doe';
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
-var gree3 = 'Good afternoon ';
-var gree4 = 'Good evening ';
-var gree5 = 'Good evening ';
-var gree6 = 'Good evening ';
+var gree1 = 'Go to Sleep Mili! ';
+var gree2 = 'Good morning Mili! ';
+var gree3 = 'Good afternoon Mili.';
+var gree4 = 'Good evening Mili.';
+var gree5 = 'Good evening Mili.';
+var gree6 = 'Good evening Mili.';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
